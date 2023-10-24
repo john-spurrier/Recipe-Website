db992c5d52e8c9495aa0ef4d75be7e685a663468
stevenhu9 <stevenhu9@yahoo.com>
2023-10-23 21:06:55 -0400

    Frontend display of items from backend

--- client/src/Search.js
+++ client/src/Search.js
@@ -5,18 +5,30 @@ function Search() {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
   const [ingredients, setingredients] = useState(['orange', 'celery']);
+  const itemsPerPage = 10;
+  const [currentPage,setCurrentPage] = useState(1);
 
   const handleSearch = async () => {
     console.log("ENTERING HANDLESEARCH()");
     // Implement your search logic here
     try{
-      const response = await axios.post('http://localhost:3001/api/query', ['orange', 'celery']);
+      const response = await axios.post('http://localhost:3001/api/query', ingredients);
 
       setResults(response.data);
-
+      console.log(results);
     } catch(error){
       console.error(error);
     }
+  }
+
+  // code for splitting the array up into pages that contain 10 items per page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = results.slice(startIndex,endIndex);
+  const totalpages = Math.ceil(results.length / itemsPerPage);
+
+  const goToPage = (page) => {
+    setCurrentPage(page);
   }
 
   return (
@@ -29,8 +41,28 @@ function Search() {
         placeholder="Search..."
       />
       <button onClick={handleSearch}>Search</button>
-    
-      
+
+      {results.length > 0 && (
+        <div>
+        <ul>
+          {currentItems.map((item,index) => (
+            <li key = {index} className = "smallText"> 
+              {item[0]} - {item[7]} 
+            </li>
+          ))}
+        </ul>
+
+        <div>
+          <button onClick={() => goToPage(currentPage - 1)} disabled = {currentPage === 1}>
+            Previous
+          </button>
+          <span>Page {currentPage} of {totalpages} </span>
+          <button onClick = {() => goToPage(currentPage + 1)} disabled = {currentPage === totalpages}>
+            Next
+          </button>
+        </div>
+        </div>
+      )}
     </div>
   );
 }

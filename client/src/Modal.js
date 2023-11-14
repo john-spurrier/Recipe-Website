7e9bd32ffe40514f62112ea9143a526b49b46fca
stevenhu9 <stevenhu9@yahoo.com>
2023-11-14 15:59:15 -0500

    fixed bugs in save function

--- client/src/Modal.js
+++ client/src/Modal.js
@@ -7,21 +7,22 @@ const Modal = ({ item, onClose }) => {
 
   useEffect(() => {
     const savedOptions = JSON.parse(sessionStorage.getItem('savedOptions')) || [];
-    setIsSaved(savedOptions.some((itm) => itm.uniqueIdentifier === item.uniqueIdentifier));
-  }, [item.uniqueIdentifier]);
+    setIsSaved(savedOptions.some((itm) => itm[1] === item[1]));
+  }, [item.name]);
 
   const handleSave = (item) => {
+    const savedOptions = JSON.parse(sessionStorage.getItem('savedOptions')) || [];
 
     if(isSaved)
     {
-      const savedOptions = JSON.parse(sessionStorage.getItem('savedOptions')) || [];
-      const updatedSavedOptions = savedOptions.filter((itm) => itm.uniqueIdentifier !== item.uniqueIdentifier);
+      const updatedSavedOptions = savedOptions.filter((itm) => {
+        return itm[1] !== item[1];
+    });
       sessionStorage.setItem('savedOptions', JSON.stringify(updatedSavedOptions));
       setIsSaved(false);
     }
     else
     {
-      const savedOptions = JSON.parse(sessionStorage.getItem('savedOptions')) || [];
       savedOptions.push(item);
       sessionStorage.setItem('savedOptions', JSON.stringify(savedOptions));
       setIsSaved(true);
@@ -34,14 +35,16 @@ const Modal = ({ item, onClose }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <button className = "modal-save" onClick = {onClose}>
+        <button className = "modal-close" onClick = {onClose}>
           Close
         </button>
-        <button className="modal-close" onClick={() => handleSave(item)}>
-          {isSaved ? "Saved": "Save"}
+        <button className="modal-save-button" onClick={() => handleSave(item)}>
+          {isSaved ? "Unsave": "Save"}
         </button>
         <h2>{item[0]}</h2>
         <p>{item[7]}</p>
+        <h2>{"Recipe Steps"}</h2>
+        <hr style = {{ borderTop: '2px solid black', width: '100%', margin:0}}/>
         <div className = "modal-scrollable-content">
           {finalElements.map((desc,index) => (
             <li key = {index}>{desc}</li>
